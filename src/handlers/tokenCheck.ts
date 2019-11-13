8a9d1a1005e9f3d2efac636528e5a9a985ff1da5
John O'Sullivan <john@eximchain.com>
2019-11-12 19:34:15 -0500

    Removed dead comment, indent cleanup

--- src/handlers/tokenCheck.ts
+++ src/handlers/tokenCheck.ts
@@ -8,20 +8,20 @@ interface APIGatewayAuthorizerEvent {
   authorizationToken: string
   methodArn: string
 }
-const TokenCheck = async (event:APIGatewayAuthorizerEvent, content:any, callback:Function) => {
+const TokenCheck = async (event: APIGatewayAuthorizerEvent, content: any, callback: Function) => {
   const token = event.authorizationToken as string;
   try {
-      const tokenInfo = await GitHub.apps.checkToken({ access_token: token, client_id: githubClientId })
-      // Assumption is that awaiting a 404 (which is what happens when token is invalid)
-      // will also throw an error, so this try-catch handles the logic.
-      callback(null, generatePolicy('user', 'Allow', event.methodArn, {
-        // Including this ensures that the authorized handler (deployStart)
-        // already has all of the user's profile data (username, email, 
-        // repo url) right when it's executed.
-        githubTokenInfo: JSON.stringify(tokenInfo)
-      }))
+    // Assumption is that awaiting a 404 (which is what happens when token is invalid)
+    // will also throw an error, so this try-catch handles the logic.
+    const tokenInfo = await GitHub.apps.checkToken({ access_token: token, client_id: githubClientId })
+    callback(null, generatePolicy('user', 'Allow', event.methodArn, {
+      // Including this ensures that the authorized handler (deployStart)
+      // already has all of the user's profile data (username, email, 
+      // repo url) right when it's executed.
+      githubTokenInfo: JSON.stringify(tokenInfo)
+    }))
   } catch (err) {
-    console.log('Error on token check: ',err);
+    console.log('Error on token check: ', err);
     // TODO: Validate we got a 404, log actual errors differently
     callback("Error: Invalid Token");
   }
@@ -38,23 +38,23 @@ interface PolicyDocument {
   Statement: IAMStatement[]
 }
 
-type AuthContext = Record<string, string|number|boolean>;
+type AuthContext = Record<string, string | number | boolean>;
 
 // This interfaace is based on the API Gateway Custom Authorizer
 // docs found here: https://github.com/awsdocs/amazon-api-gateway-developer-guide/blob/master/doc_source/api-gateway-lambda-authorizer-output.md
 interface AuthorizerResponse {
   principalId: string
   policyDocument?: PolicyDocument
-  context?:AuthContext
+  context?: AuthContext
 }
 // Help function to generate an IAM policy, courtesy of
 // https://github.com/awsdocs/amazon-api-gateway-developer-guide/blob/master/doc_source/apigateway-use-lambda-authorizer.md#example-create-a-token-based-lambda-authorizer-function
-var generatePolicy = function(principalId:string, effect:'Allow'|'Deny', resource:string, context?:AuthContext) {
+var generatePolicy = function (principalId: string, effect: 'Allow' | 'Deny', resource: string, context?: AuthContext) {
   var authResponse = {} as AuthorizerResponse;
-  
+
   authResponse.principalId = principalId;
   if (effect && resource) {
-    const policyDocument:PolicyDocument = {
+    const policyDocument: PolicyDocument = {
       Version: '2012-10-17',
       Statement: [{
         Action: 'execute-api:Invoke',
@@ -64,13 +64,8 @@ var generatePolicy = function(principalId:string, effect:'Allow'|'Deny', resourc
     };
     authResponse.policyDocument = policyDocument;
   }
-  
+
   // Optional output with custom properties of the String, Number or Boolean type.
-  // authResponse.context = {
-  //     "stringKey": "stringval",
-  //     "numberKey": 123,
-  //     "booleanKey": true
-  // };
   if (context) authResponse.context = context;
   return authResponse;
 }
