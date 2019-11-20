a875fa3101b20286cc465a62a4a90ce6fc475541
John O'Sullivan <john@eximchain.com>
2019-11-20 16:36:20 -0500

    Setup GET handler for basic client-side DynamoDB validation

--- src/handlers/deployStart.ts
+++ src/handlers/deployStart.ts
@@ -1,24 +1,39 @@
 import uuid from 'uuid/v4';
 import { isDeployArgs, newDeployArgs } from '@eximchain/ipfs-ens-types/spec/deployment';
+import { ReadDeployment } from '@eximchain/ipfs-ens-types/spec/methods/private';
 import { APIGatewayEvent } from '@eximchain/api-types/spec/events';
 import { S3, DynamoDB, CodePipeline } from '../services';
-import { userErrorResponse, unexpectedErrorResponse, successResponse } from '@eximchain/api-types/spec/responses';
+import { userErrorResponse, unexpectedErrorResponse, successResponse, HttpMethods } from '@eximchain/api-types/spec/responses';
 
-const DeployStart = async (event: APIGatewayEvent) => {
+const DeployProxyApi = async (event: APIGatewayEvent) => {
   console.log("DeployStart request: " + JSON.stringify(event));
+  const deployName = event.pathParameters['proxy'];
+  const method = event.httpMethod.toUpperCase() as HttpMethods.ANY;
+  switch (method) {
+    case 'OPTIONS':
+      return successResponse(null);
+    case 'POST':
+      const body = JSON.parse(event.body || '');
+      const authToken = event.headers['Authorization']
+      return createDeploy(body, authToken);
+    case 'GET':
+      return getDeploy(deployName);
+    default:
+      return userErrorResponse({ message: `Unrecognized HTTP method: ${method}` })
+  }
+}
 
-  // Validate arguments
-  const body = event.body ? JSON.parse(event.body) : {};
-  if (!isDeployArgs(body)) return userErrorResponse({
+async function createDeploy(args: any, oauthToken: string) {
+  if (!isDeployArgs(args)) return userErrorResponse({
     message: `Please include all of the required keys: ${Object.keys(newDeployArgs()).join(', ')}`
   })
-  const { ensName, packageDir, buildDir, owner, repo, branch } = body;
+  const { ensName, packageDir, buildDir, owner, repo, branch } = args;
 
   try {
     // Initialize DeployItem in DynamoDB
-    throw new Error(`Bailing out of logic for testing, deployStart received following args: ${JSON.stringify(body, null, 2)}`);
+    throw new Error(`Bailing out of logic for testing, deployStart received following arg body: ${JSON.stringify(args, null, 2)}`);
     const deploymentSuffix = uuid();
-    const newItem = await DynamoDB.initDeployItem(body);
+    const newItem = await DynamoDB.initDeployItem(args);
 
     // Create new CodePipeline's artifact bucket
     const artifactBucketname = `ipfs-ens-artfacts-${deploymentSuffix}`;
@@ -27,13 +42,25 @@ const DeployStart = async (event: APIGatewayEvent) => {
     // Create the CodePipeline with GitHub & S3 Source
     // based on the provided owner/repo/branch.
     const pipelineName = `ipfs-ens-builder-${deploymentSuffix}`;
-    const oauthToken = event.headers['Authorization']
     const createdPipeline = await CodePipeline.createDeploy(ensName, pipelineName, packageDir, buildDir, oauthToken, owner, repo, branch)
     return successResponse({ newItem, createdBucket, createdPipeline });
   } catch (err) {
     return unexpectedErrorResponse(err);
   }
-
 }
 
-export default DeployStart;
+async function getDeploy(deployName: string) {
+  try {
+    const item = await DynamoDB.getDeployItem(deployName);
+    const result = item ? {
+      item, exists: true
+    } : {
+        item, exists: false
+      }
+    return successResponse(result);
+  } catch (err) {
+    return unexpectedErrorResponse(err);
+  }
+}
+
+export default DeployProxyApi;
